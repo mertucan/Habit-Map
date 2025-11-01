ed18b6f74903d33dc6ea2930b1792f08a9219295
Emre345 <emredemirkaya8951@gmail.com>
2025-11-01 21:35:07 +0300

    a

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 from .extensions import db, bcrypt
 from datetime import datetime
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
